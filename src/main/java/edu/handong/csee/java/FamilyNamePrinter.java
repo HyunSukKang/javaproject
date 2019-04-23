2bb7d90822e840a330802df5db5d7013a8b3e905
HyunSukKang <21400018@handong.edu>
2019-04-23 13:15:38 +0900

    Commit for gradle setting

---
+++ src/main/java/edu/handong/csee/java/FamilyNamePrinter.java
@@ -0,0 +1,28 @@
+package edu.handong.csee.java;
+
+public class FamilyNamePrinter {
+    private String[] stringNames;
+    private String[] familyNames;
+
+    
+    public FamilyNamePrinter(String names){
+        stringNames = names.split(", ");
+        familyNames = new String[stringNames.length];
+    }
+    public void printFamilyNames(){
+        for(String name : stringNames){
+            System.out.println(name);
+        }
+        for(int i=0; i<stringNames.length; i++){
+            familyNames[i] = stringNames[i].trim().split(" ")[1];
+        }
+        for(String familyName:familyNames){
+            System.out.println(familyName);
+        }
+    }
+
+    public static void main(String[] args){
+        FamilyNamePrinter FNPrinter = new FamilyNamePrinter(args[0]);
+        FNPrinter.printFamilyNames();
+    }
+}
